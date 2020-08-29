3bf9c78b60c04e5ddf0eafa82b07107319f35324
Stekovaya <tully.f63@liberiaom.com>
2020-08-29 02:50:32 +0000

    first

---
+++ index.js
@@ -0,0 +1,44 @@
+var https = require("https");
+var fs = require("fs");
+process.argv.shift();
+process.argv.shift();
+var arg = process.argv;
+var exit = process.exit;
+if(arg.length == 0){
+  console.error("Missing arguments");
+  exit(1);
+}else{
+  if(arg[0] == "help" || arg[0] == "h"){
+    console.log(`OPM v1.0.0
+ - install <name> / i <name>
+   Install package
+ - help / h
+   Display help`);
+    exit(0);
+  }else if(arg[0] == "install" || arg[0] == "i"){
+    if(!arg[1]){
+      console.error("Missing argument: <name>");
+      exit(1);
+    }
+    console.log("    Connecting...");
+    https.get("https://library.obratnaya.repl.co/" + arg[1] + ".oba",(res)=>{
+      console.log("\x1b[1A\x1b[1M\x1b[41m\x1b[30m\x1b[1m √ \x1b[m Connected!");
+      if(res.statusCode != 200){
+        console.log("\x1b[43m\x1b[30mERR\x1b[m Package not found!");
+        exit(1);
+      }
+      var d = "";
+      res.on("data",c=>{
+        d+=c;
+      }).on("end",()=>{
+        console.log("\x1b[41m\x1b[30m\x1b[1m √ \x1b[m Done!");
+        console.log(`Name    : ${arg[1]}
+Filename: ${arg[1]}.oba`);
+        fs.writeFileSync(arg[1] + ".oba",d);
+        exit(0);
+      });
+    }).on("error",()=>{
+      
+    });
+  }
+}
